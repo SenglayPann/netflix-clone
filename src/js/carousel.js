addbaa0baad827b382a1cddcb9a51bbde30b1cc1
Pann Senglay <senglaypann@gmail.com>
2024-04-24 20:39:46 +0700

    test: test review board 2

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,1 +1,26 @@
-document.querySelector()
+//find the handle
+
+document.addEventListener("click", e => {
+    let handle;
+    if (e.target.matches(".handle")) {
+        handle = e.target;
+    }
+    else {
+        handle = e.target.closest(".handle");
+    }
+    if (handle != null) onHandleClick(handle);
+});
+
+//functions when click on the handle
+
+function onHandleClick(handle) {
+    const slider = handle.closest(".carousel-container").querySelector(".slider");
+    const sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue("--slider-index"));
+    if (handle.classList.contains("left-handle")) {
+        slider.style.setProperty("--slider-index", sliderIndex - 1)
+    };
+
+    if (handle.classList.contains("right-handle")) {
+        slider.style.setProperty("--slider-index", sliderIndex + 1)
+    };
+};
