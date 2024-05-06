0c8cc2d0dcecca0ff30586c9bac5c6016f16ea17
Pann Senglay <senglaypann@gmail.com>
2024-05-06 21:52:14 +0700

    add: added some changes to modal (70% done)

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,22 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const button = document.getElementsByClassName('button');
-    const modalContainer = document.getElementById('modal-container');
-    const modal = document.getElementById('modal');
+  const openButtons = document.getElementsByClassName('modalOpenButton');
+  const modalContainer = document.getElementById('modal-container');
+  const modal = document.getElementById('modal');
+  const modalClosingButton = document.getElementById("closing-button");
 
-    button[0].addEventListener('click', () => {
+  const setOpen = (button) => {
+    button.addEventListener('click', () => {
       modalContainer.style.zIndex = 9999;
       modalContainer.style.opacity = 1;
       modalContainer.style.scale = 1;
       modal.style.scale = 1;
       modal.style.opacity = 1;
     })
+  }
 
-    modalContainer.addEventListener('click', (e) => {
-      if (e.target.classList.contains('modal-container')) {
+  const setClose = (button) => {
+    button.addEventListener('click', (e) => {
+      if (e.target.classList.contains('modal-container') || e.target.classList.contains('closing-button')) {
         modalContainer.style.zIndex = -9999;
         modalContainer.style.opacity = 0;
-        modal.style.scale = .8;
+        modal.style.scale= .8;
         modal.style.opacity = 0;
       }
-    })
-  });
+    });
+  }
+
+  // setClose(modalContainer);
+  setClose(modalClosingButton)
+
+  // setOpen(modalClosingButton);
+
+  for (let i = 0; i < openButtons.length; i++) {
+    setOpen(openButtons[i]);
+  }
+
+});
